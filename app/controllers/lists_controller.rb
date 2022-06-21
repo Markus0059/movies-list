9b7151b82b53fcd89e08aeb13566ba3a2b9d69b5
Germe Marc Antoine <markus0059@hotmail.fr>
2022-06-21 14:51:00 +0200

    Add dotenv - Protect my secret data in .env file

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_param)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def list_param
+    params.require(:list).permit(:name)
+  end
+
+end
